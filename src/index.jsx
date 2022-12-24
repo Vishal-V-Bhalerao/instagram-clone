550885ca8eb49fe39ec6711accaab3b4ae5ce4f8
Vishal Bhalerao <vishalbhalerao.bhalerao@gmail.com>
2022-12-24 18:09:04 +0530

    login page ui

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import fireBaseContext from './context/firebase';
-import { firebase, FieldValue } from './lib/firebase';
+import { firebase, db, FieldValue, analytics,  getAuth, createUserWithEmailAndPassword } from './lib/firebase';
 import './styles/app.css'
 // eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <fireBaseContext.Provider value={{ firebase, FieldValue }}>
+  <fireBaseContext.Provider value={{ firebase, db, FieldValue, analytics, getAuth, createUserWithEmailAndPassword }}>
     <App></App>
   </fireBaseContext.Provider>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
     content: ["./src/**/*.{html,js,jsx}"],
     theme: {
+      fill: (theme) => ({
+        red: theme("colors.red.primary")
+      }),
       extend: {},
+      colors:{
+        white:"#ffff",
+        blue:{
+          medium: "#005c9a"
+        },
+        black:{
+          light: "#005c98",
+          faded:"#0000059"
+        },
+        gray:{
+          base: "#616161",
+          background: "##fafafa",
+          primary:"#dbdbdb"
+        },
+        red:{
+          primary: "#ed4956"
+        }
+      }
     },
     plugins: [],
   }
